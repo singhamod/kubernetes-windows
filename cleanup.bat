7badc7ade76dc7ae4bc0d1ea7a4fec8afe5df778
gamodg <gamodg@gmail.com>
2020-01-30 16:18:08 +0530

    Update cleanup.bat

--- cleanup.bat
+++ cleanup.bat
@@ -1,11 +1,5 @@
 @echo off
 
-echo 'K8s: clean up'
-call kubectl delete -f  configs\ >NUL
-
-echo 'Reverse Proxy: stop'
-call tasklist | find /i "nginx.exe" && taskkill /im nginx.exe /F >NUL
-
 echo 'Logging: cleanup'
 call kubectl delete namespace logging >NUL
 call kubectl delete -f  logging\ >NUL
